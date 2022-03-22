d7596fb5a78e364215faefe1ff60fb11d0db31ed
Nimrod Astarhan <nimrod@codota.com>
2022-03-22 00:52:48 -0500

    latest

--- source/content.js
+++ source/content.js
@@ -1,22 +1,61 @@
 import hljs from 'highlight.js';
 
+hljs.addPlugin({
+	'after:highlightElement': ({ el, result }) => {
+		// move the language from the result into the dataset
+		// el.dataset.language = result.language;
+		console.log(el);
+	}
+});
+
+const languages = [
+	"python",
+	"html",
+	"bash",
+	"css",
+	"ruby",
+	"java",
+	"javascript",
+	"php",
+	"sql",
+	"c#",
+	"c",
+	"arduino"
+]
+
+function getRandomLanguage() {
+	return languages[Math.floor(Math.random() * languages.length)];
+}
+
+function updateUI() {
+	for (const element of document.querySelectorAll('p')) {
+		element.className = getRandomLanguage();
+		hljs.highlightElement(element);
+	}
+
+	for (const element of document.querySelectorAll('i')) {
+		element.className = getRandomLanguage();
+		hljs.highlightElement(element);
+	}
+
+	for (const element of document.querySelectorAll('span')) {
+		element.className = getRandomLanguage();
+		hljs.highlightElement(element);
+	}
+
+	for (const element of document.querySelectorAll('tr.mslg')) {
+		element.className = getRandomLanguage();
+		hljs.highlightElement(element);
+	}
+}
+
 async function init() {
-	// Hljs.configure({
-	// 	cssSelector: 'p'
-	// });
-	//
-	// hljs.highlightAll();
 
 	document.addEventListener('DOMContentLoaded', event => {
-		for (const element of document.querySelectorAll('p')) {
-			element.className = 'python';
-			hljs.highlightElement(element);
-		}
+		updateUI();
 	});
 
-	// Document.querySelectorAll('p').forEach((el) => {
-	// 	hljs.highlightElement(el);
-	// });
+	updateUI();
 
 	// const observer = new MutationObserver(updateUI);
 	// const observeFragment = (): void => {
@@ -32,7 +71,7 @@ async function init() {
 	//
 	// updateUI();
 	// observeFragment();
-	// document.addEventListener('pjax:end', updateUI); // Update on page change
+	document.addEventListener('pjax:end', updateUI); // Update on page change
 	// document.addEventListener('pjax:end', observeFragment);
 }
 
